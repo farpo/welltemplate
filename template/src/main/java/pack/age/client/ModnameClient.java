4940fa63f848bfa35b3e1272e52cff9534195516
farpo <farpoproject@gmail.com>
2026-04-08 16:26:35 +0200

    Remove vestiges

--- template/src/main/java/pack/age/client/ModnameClient.java
+++
@@ -1,9 +0,0 @@
-package ``MOD_GROUP``.client;
-
-import net.fabricmc.api.ClientModInitializer;
-public class ``ENTRYPOINT_NAME``Client implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-
-    }
-}
--- template/src/main/java/pack/age/network/ReceiverPacket.java
+++
@@ -1,7 +0,0 @@
-package ``MOD_GROUP``.network;
-
-import net.minecraft.network.packet.CustomPayload;
-
-public interface ReceiverPacket<C> extends CustomPayload {
-    void receive(C context);
-}
